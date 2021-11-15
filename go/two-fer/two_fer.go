cb893f50d4d259024b04827fe7fb34e12c797cd6
Henry Zhang <henry@thinkific.com>
2021-11-14 20:47:42 -0800

    initial commit

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,15 @@
+// Package twofer creates a sentence.
+package twofer
+
+import (
+	"fmt"
+)
+
+// ShareWith returns a sentence of the form "One for X, one for me."
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
